ce3e54a3d6c09ff1d38c815ecb156b3cc8cbd0c1
pramodC135 <pramodmadhushan4@gmail.com>
2024-09-25 00:10:23 +0530

    added bearer token changes

--- Yarp.Auth.Gateway/Program.cs
+++ Yarp.Auth.Gateway/Program.cs
@@ -1,10 +1,32 @@
+using Microsoft.AspNetCore.Authentication.BearerToken;
+using System.Security.Claims;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services
+    .AddAuthentication(BearerTokenDefaults.AuthenticationScheme)
+    .AddBearerToken();
+
 var app = builder.Build();
+
+app.MapGet("login", () =>
+    Results.SignIn(
+        new ClaimsPrincipal(
+            new ClaimsIdentity(
+                [
+                    new Claim("sub", Guid.NewGuid().ToString())
+                ],
+                BearerTokenDefaults.AuthenticationScheme)),
+        authenticationScheme: BearerTokenDefaults.AuthenticationScheme
+        ));
+
+app.UseAuthentication();
+
+app.UseAuthorization();
 
 app.MapReverseProxy();
 
